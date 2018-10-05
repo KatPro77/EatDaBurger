8895282ae959b2d63a9ff82b3c662d95c0b5fd86
Katerina Stanley <katerinastanley77@gmail.com>
2018-10-05 13:39:06 -0400

    config updates

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ("Spicy Bufalo Burger");
+INSERT INTO burgers (burger_name) VALUES ("Black Bean Burger");
